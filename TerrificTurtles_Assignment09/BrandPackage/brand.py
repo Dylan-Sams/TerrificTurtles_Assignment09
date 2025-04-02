fc4d0831b0424db42bec8179891f12a3ef5a5d05
oalkhawaga <oalkhawaga@outlook.com>
2025-04-02 19:19:18 -0400

    Omar's Commit steps 5 and 6

--- TerrificTurtles_Assignment09/BrandPackage/brand.py
+++ TerrificTurtles_Assignment09/BrandPackage/brand.py
@@ -5,12 +5,9 @@
 # Due Date:   4/2/2025
 # Course #/Section:   IS4010-001
 # Semester/Year:   Spring 2025
-# Brief Description of the assignment:  {required}
-
-# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
-# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
-
-# Anything else that's relevant:
+# Brief Description of the assignment:  This assignment is working  in teams to create a project that accesses our SQL Server and extracts some data from the Grocery Store Simulator database, and produces some results.
+# Brief Description of what this module does. This module takes a brandID and wants to looks up the brand name
+# Citations: 
 
 
 import random
@@ -41,3 +38,22 @@ class ProductService:
         Returns:pyodbc.Row: A randomly selected product row.
         """
         return random.choice(product_list)
+
+    def __init__(self, connection):
+        self.connection = connection
+
+    def get_brand_name(self, brand_id):
+        """
+        Fetches the brand name from the tbrand table using a given ID.
+
+        Parameters:brand_id (int): The ID of the brand.
+
+        Returns:str: The name of the brand if found, otherwise "Unknown".
+        """
+        cursor = self.connection.cursor()
+        query = f"SELECT Brand FROM tBrand WHERE BrandID = {brand_id}"
+        cursor.execute(query)
+        result = cursor.fetchone()
+        cursor.close()
+        return result.Manufacturer if result else "Unknown"
+    
--- TerrificTurtles_Assignment09/sumOfItemsSoldPackage/sumOfItemsSold.py
+++ TerrificTurtles_Assignment09/sumOfItemsSoldPackage/sumOfItemsSold.py
@@ -5,9 +5,36 @@
 # Due Date:   4/2/2025
 # Course #/Section:   IS4010-001
 # Semester/Year:   Spring 2025
-# Brief Description of the assignment:  {required}
+# Brief Description of the assignment:  This assignment is working  in teams to create a project that accesses our SQL Server and extracts some data from the Grocery Store Simulator database, and produces some results.
+# Brief Description of what this module does. This module takes a productID and sees how many of that product have been sold
+# Citations: https://stackoverflow.com/questions/75348376/executing-a-sql-query-using-python
+# Anything else that's relevant:N/A
 
-# Brief Description of what this module does. {Do not copy/paste from a previous assignment. Put some thought into this. required}
-# Citations: {"Stack Overflow" is not sufficient. Provide repeatable links, book page #, etc.}
 
-# Anything else that's relevant:
+from databaseManagementPackage.databaseManagement import databaseManagement 
+
+def get_items_sold(product_id):
+    """
+    Fetches the number of items sold for the given product
+    """
+    db = databaseManagement()  
+    conn = db.connect_to_database()  
+
+
+    cursor = conn.cursor()
+    query = f"""
+        SELECT SUM(dbo.tTransactionDetail.QtyOfProduct) AS NumberOfItemsSold
+        FROM dbo.tTransactionDetail
+        INNER JOIN dbo.tTransaction 
+        ON dbo.tTransactionDetail.TransactionID = dbo.tTransaction.TransactionID
+        WHERE dbo.tTransaction.TransactionTypeID = 1 
+        AND dbo.tTransactionDetail.ProductID = {product_id}
+    """
+
+    cursor.execute(query)
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return result[0] if result and result[0] is not None else 0
